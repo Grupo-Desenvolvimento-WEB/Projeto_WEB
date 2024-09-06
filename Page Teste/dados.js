077b71f85ae667347ca5bb4d5f3479fb41095ee9
✨Bruna ✨ <163605337+Bru67@users.noreply.github.com>
2024-09-06 20:05:43 -0300

    commit

--- Page Teste/dados.js
+++ Page Teste/dados.js
@@ -1,52 +1,62 @@
 
 // Criando uma lista de objetos;
-
 let destinos = [
     {
-    titulo: "Barramas",
-    imagem: "barramas.jpg",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Rio de Janeiro",
-    imagem: "", //not working
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Maceió",
-    imagem: "barramas3.jpg",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Gramado",
-    imagem: "../",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Porto Seguro",
-    imagem: "../",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Tokyo",
-    imagem: "../",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Irlanda",
-    imagem: "../",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Barcelona",
-    imagem: "../",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}, 
-{
-    titulo: "Paris",
-    imagem: "../",
-    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacinia dignissim nulla, eget volutpatn sapien suscipit euismod."
-}];
+        titulo: "Barramas",
+        imagem: "barramas.jpg",
+        tags: "",
+        descricao: "Descubra a beleza exótica de Barramas, onde praias de areia branca e águas cristalinas esperam por você."
+    }, 
+    {
+        titulo: "Rio de Janeiro",
+        imagem: "", 
+        tags: "",
+        descricao: "Explore o vibrante Rio de Janeiro, famoso por seu carnaval, suas praias icônicas e a impressionante estátua do Cristo Redentor."
+    }, 
+    {
+        titulo: "Maceió",
+        imagem: "",
+        tags: "",
+        descricao: "Maceió oferece um cenário tropical com suas lagoas azuis e praias encantadoras, ideal para relaxar e desfrutar do sol."
+    }, 
+    {
+        titulo: "Gramado",
+        imagem: "../",
+        tags: "",
+        descricao: "A charmosa Gramado é conhecida por seu clima europeu, arquitetura pitoresca e festivais sazonais encantadores."
+    }, 
+    {
+        titulo: "Porto Seguro",
+        imagem: "../",
+        tags: "",
+        descricao: "Porto Seguro é o destino perfeito para quem busca diversão e cultura, com suas praias deslumbrantes e rica história."
+    }, 
+    {
+        titulo: "Tokyo",
+        imagem: "../",
+        tags: "",
+        descricao: "Tokyo combina a modernidade com a tradição, oferecendo uma experiência única com seus templos antigos e arranha-céus futuristas."
+    }, 
+    {
+        titulo: "Irlanda",
+        imagem: "../",
+        tags: "",
+        descricao: "A Irlanda é conhecida por suas paisagens verdes, castelos históricos e a calorosa hospitalidade de seus habitantes."
+    }, 
+    {
+        titulo: "Barcelona",
+        imagem: "../",
+        tags: "",
+        descricao: "Barcelona encanta com sua arquitetura deslumbrante de Gaudí, suas praias e uma vibrante cena cultural e gastronômica."
+    }, 
+    {
+        titulo: "Paris",
+        imagem: "../",
+        tags: "",
+        descricao: "Paris, a cidade das luzes, é famosa por seus monumentos icônicos, museus de renome e uma rica tradição de café e gastronomia."
+    }
+];
+
 
 // // Função para criar um elemento <img> e adicioná-lo ao DOM
 // function exibirImagem(destinos) {
--- Page Teste/page.js
+++ Page Teste/page.js
@@ -4,13 +4,30 @@ function pesquisar() {
 
     let section = document.getElementById("resultado-pesquisa") //Pega o dado com o id e armazena em uma variável
 
-    let resultado = "" //Cria uma string vazia para exibir os resultados da pesquisa
+    let campoPesquisa = document.getElementById("campo-pesquisa").value //Armazena o resultado do input
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Insira o conteúdo no campo de pesquisa!</p>"
+        return // Não retorna nada se o campo estiver vazio
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultado = ""; //Cria uma string vazia para exibir os resultados da pesquisa
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     //Interage com a lista de objetos do arquivo dados.js
     for (let destino of destinos) {
-        resultado += `
-        <h1>${destino.titulo}</h1>
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        titulo = destino.titulo.toLowerCase()
+        descricao = destino.descricao.toLowerCase()
+        tags = destino.tags.toLowerCase()
+        //Se houver um objeto condizente com o input de pesquisa, ele cria o objeto e mostra na página html
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) ) {
+            resultado += `
+            <h1>${destino.titulo}</h1>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
             <div class="carousel-item active">
                 <img class="d-block w-100" src=${destino.imagem} alt="Imagem-destino">
@@ -19,7 +36,16 @@ function pesquisar() {
             <div class="descricao">
             <p>${destino.descricao}</p>
             </div> `
+
+            
+        }
+
     }
+
+    if (!resultado){
+        resultado = "<p>Nenhum resultado encontrado!</p>"
+    }
+
     section.innerHTML = resultado //Substitui elementos no arquivo html
 
 }
