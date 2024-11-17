d852de325bfa5bd475749977744860b707a65f52
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2024-11-17 13:52:37 -0300

    Manutenção do card | Tentativa de fazer a imagem carregar corretamente

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "Root#963", //Alterar a senha conforme a máquina que está rodando o programa
+    user: "developer",
+    password: "1234567", //Alterar a senha conforme a máquina que está rodando o programa
     database: "Projeto_Web"
 });
 
@@ -106,16 +106,29 @@ router.delete('/api/usuario/:id', (req, res) => {
 
 //TABELA PACOTE - GET
 router.get('/api/pacote', (req, res) => {
-    const query = 'SELECT id_pacote, imagem, titulo, descricao, preco, num_compras FROM pacote'; 
+    const query = 'SELECT id_pacote, titulo, descricao, preco, imagem FROM pacote';
+
     db.query(query, (err, results) => {
         if (err) {
-            console.error('Erro ao buscar dados:', err);
-            res.status(500).send('Erro ao buscar dados');
+            console.error('Erro ao buscar pacotes:', err);
+            res.status(500).send('Erro ao buscar pacotes');
             return;
         }
-        res.json(results);
+
+        // Converte cada BLOB de imagem em Base64
+        const pacotes = results.map(pacote => {
+            if (pacote.imagem) {
+                // Converte o BLOB para uma string Base64
+                const imagemBase64 = Buffer.from(pacote.imagem).toString('base64');
+                pacote.imagem = `data:image/jpeg;base64,${imagemBase64}`;
+            }
+            return pacote;
+        });
+
+        res.json(pacotes); // Retorna os pacotes com as imagens convertidas
     });
 });
+
 
 //endpoint para capturar um pacote por id
 router.get('/api/pacote/:id_pacote', (req, res) => {
